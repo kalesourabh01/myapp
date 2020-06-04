e369705902d58d54c8be050b685b59d68a049c7b
Sourabh Kale <kalesourabh01@gmail.com>
2020-06-04 15:36:10 +0530

    form changes completed

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -19,22 +19,32 @@ class BasicForms extends React.Component {
   constructor(){
     super();
     this.state ={
-      fireRedirect: false
+      email: '', pass: ''
     }
   }
 
+  handleChange = (event) =>{
+    this.setState({
+      [event.target.name]: event.target.value
+  })
+  };
+
   handleSubmit = (event) => {
     event.preventDefault();
-    this.props.history.push('/dashboard');
+    if(this.state.email == 'kalesourabh01@gmail.com' && this.state.pass == 123){
+      this.props.history.push('/dashboard');
+    } else{
+      alert('enter valid creds');
+    }
+    
   };
 
 
   render() {
-    const { fireRedirect } = this.state;
     return <Container className="col-lg-6"> <Form  onSubmit={this.handleSubmit}> 
     <Form.Group controlId="formBasicEmail">
       <Form.Label>Email address</Form.Label>
-      <Form.Control type="email" placeholder="Enter email" required />
+      <Form.Control type="email" placeholder="Enter email" name= "email" required onChange={this.handleChange} />
       <Form.Text className="text-muted">
         We'll never share your email with anyone else.
       </Form.Text>
@@ -42,7 +52,7 @@ class BasicForms extends React.Component {
   
     <Form.Group controlId="formBasicPassword">
       <Form.Label>Password</Form.Label>
-      <Form.Control type="password" placeholder="Password" />
+      <Form.Control type="password" placeholder="Password"  name= "pass" onChange={this.handleChange}/>
     </Form.Group>
     <Form.Group controlId="formBasicCheckbox">
       <Form.Check type="checkbox" label="Check me out" />
