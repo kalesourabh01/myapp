1fb1a242ba74b7700a5aafd3ad4258ef6147a74a
Sourabh Kale <kalesourabh01@gmail.com>
2020-06-04 17:31:39 +0530

    added dashboard

--- src/components/dashboard/Dashboard.jsx
+++ src/components/dashboard/Dashboard.jsx
@@ -1,9 +1,6 @@
 import React from 'react';
-import { Button} from 'react-bootstrap';
-import { Form} from 'react-bootstrap';
-import { Container} from 'react-bootstrap';
-
-
+import { Button, Row, Col, Card, Navbar, Form, Container, Nav, NavDropdown, FormControl  } from 'react-bootstrap';
+import Profile from "./Profile";
 class Dashbord extends React.Component {
   constructor(){
     super();
@@ -15,11 +12,36 @@ class Dashbord extends React.Component {
     alert('hello sourabh')
   }
 
-
   render() {
-    return <div>
-        <h1>i am on dashbord</h1>
-    </div>
+    return <Container fluid> 
+    <Row>
+    <Col>
+        <Navbar bg="gray" expand="lg" >
+        <Navbar.Brand href="#home">Welcome Sourabh</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">Link</Nav.Link>
+            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+          <Form inline>
+            <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+            <Button variant="outline-success">Search</Button>
+          </Form>
+        </Navbar.Collapse>
+      </Navbar>
+   
+      <Profile/>
+    </Col>
+    </Row>
+  </Container>
   }
 }
 
---
+++ src/components/dashboard/Profile.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import { Card, Button} from 'react-bootstrap';
+
+class profile extends Component {
+    render() {
+        return (
+            <Card style={{ width: '18rem' }}>
+        <Card.Img variant="top" src="https://instagram.fhyd11-1.fna.fbcdn.net/v/t51.2885-15/e35/60326935_329712031048232_8463915410186745562_n.jpg?_nc_ht=instagram.fhyd11-1.fna.fbcdn.net&_nc_cat=108&_nc_ohc=i1upyw90y6sAX8QcNbb&oh=35a39625090ccafd58329933ee0919ba&oe=5F01445B" />
+        <Card.Body>
+          <Card.Title>Sourabh kale</Card.Title>
+          <Card.Text>
+            Some quick example text to build on the card title and make up the bulk of
+            the card's content.
+          </Card.Text>
+          <Button variant="primary">Go somewhere</Button>
+        </Card.Body>
+       </Card>
+        );
+    }
+}
+
+export default profile;
